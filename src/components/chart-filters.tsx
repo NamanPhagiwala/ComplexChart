558c669cb501327f2b56b28e73be2ac3e60e0cbc
NamanPhagiwala <NamanPhagiwala21@gmail.com>
2024-12-13 20:44:23 +0530

    height fixes

--- src/components/chart-filters.tsx
+++ src/components/chart-filters.tsx
@@ -14,7 +14,7 @@ const ChartFilters = () => {
     const chartElement = document.getElementById("chart");
     if (chartElement) {
       if (!isFullscreen) {
-        chartElement.style.width = "80vw";
+        chartElement.style.width = "100%";
         chartElement.style.height = "100vh";
         chartElement.style.position = "fixed";
         chartElement.style.top = "0";
@@ -22,8 +22,8 @@ const ChartFilters = () => {
         chartElement.style.zIndex = "100";
         chartElement.style.backgroundColor = "white";
       } else {
-        chartElement.style.width = "1000px";
-        chartElement.style.height = "500px";
+        chartElement.style.width = "100%";
+        chartElement.style.height = "100%";
         chartElement.style.position = "relative";
         chartElement.style.top = "auto";
         chartElement.style.left = "auto";
